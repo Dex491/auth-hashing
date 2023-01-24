3052023ac5ce104adac46324f59f85436b79d949
Dave <Daverevell491@gmail.com>
2023-01-24 19:14:43 +0000

    Requirements complete

--- src/routers/registrations.js
+++ src/routers/registrations.js
@@ -1,19 +1,37 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const prisma = require('../utils/prisma.js')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const prisma = require("../utils/prisma.js");
 
-router.post('/', async (req, res) => {
-    // Get the username and password from request body
-    
-    // Hash the password: https://github.com/kelektiv/node.bcrypt.js#with-promises
-    
-    // Save the user using the prisma user model, setting their password to the hashed version
-    
-    // Respond back to the client with the created users username and id
-    res.status(201).json({ user: undefined })
+router.post("/", async (req, res) => {
+	// Get the username and password from request body
+	const { username, password } = req.body;
+	const saltRounds = 10;
+
+	// Hash the password: https://github.com/kelektiv/node.bcrypt.js#with-promises
+	bcrypt.hash(password, saltRounds, async (err, hashed_pw) => {
+		// Save the user using the prisma user model, setting their password to the hashed version
+		try {
+			const new_user = await prisma.user.create({
+				data: {
+					username,
+					password: hashed_pw,
+				},
+			});
+			delete new_user.password;
+
+			// Respond back to the client with the created users username and id
+			res.status(201).json({ user: new_user });
+		} catch (e) {
+			if (error.code === "P2002") {
+				res.status(403).json({ ewrror: "The username is already taken" });
+			} else {
+				res.status(500).json({ error: e });
+			}
+		}
+	});
 });
 
 module.exports = router;
--- src/routers/sessions.js
+++ src/routers/sessions.js
@@ -1,22 +1,44 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const prisma = require('../utils/prisma.js')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const prisma = require("../utils/prisma.js");
 
-router.post('/', async (req, res) => {
-    const { username, password } = req.body;
-    // Get the username and password from the request body
+router.post("/", async (req, res) => {
+	// Get the username and password from the request body
+	const { username, password } = req.body;
 
-    // Check that a user with that username exists in the database
-    // Use bcrypt to check that the provided password matches the hashed password on the user
-    // If either of these checks fail, respond with a 401 "Invalid username or password" error
+	// Check that a user with that username exists in the database
+	const user = await prisma.user.findUnique({
+		where: {
+			username,
+		},
+	});
 
-    // If the user exists and the passwords match, create a JWT containing the username in the payload
-    // Use the JWT_SECRET environment variable for the secret key
+	if (!user) {
+		res.status(401).json({ error: "Invalid username or password." });
+	}
 
-    // Send a JSON object with a "token" key back to the client, the value is the JWT created
+	// Use bcrypt to check that the provided password matches the hashed password on the user
+	// If either of these checks fail, respond with a 401 "Invalid username or password" error
+	bcrypt.compare(password, user.password, (err, result) => {
+		if (!result) {
+			res.status(401).json({ error: "Invalid username or password." });
+		} else {
+			// Use the JWT_SECRET environment variable for the secret key
+			const secret = process.env.JWT_SECRET;
+
+			// If the user exists and the passwords match, create a JWT containing the username in the payload
+			const access_token = jwt.sign(
+				{ sub: user.id, username: user.username },
+				secret
+			);
+
+			// Send a JSON object with a "token" key back to the client, the value is the JWT created
+			res.status(201).json({ access_token });
+		}
+	});
 });
 
 module.exports = router;
